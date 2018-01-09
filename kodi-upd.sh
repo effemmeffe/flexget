4a865b4665743886b8f9c7f729a2d71eef8137ed
effemmeffe <effemmeffe@gmail.com>
2018-01-09 13:19:55 +0100

    ini

---
+++ kodi-upd.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' http://192.168.254.100:8080/jsonrpc
+curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' http://127.0.0.1:8080/jsonrpc >/dev/null 2>&1
